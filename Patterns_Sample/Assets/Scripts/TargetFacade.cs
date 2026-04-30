a2e02397dfe327b5383b100c58ff92e5bbb75a38
mafora12 <marianaflorez1224@gmail.com>
2026-04-29 23:00:56 -0500

    chanse

--- Patterns_Sample/Assets/Scripts/TargetFacade.cs
+++ Patterns_Sample/Assets/Scripts/TargetFacade.cs
@@ -11,9 +11,14 @@ public class TargetFacade : MonoBehaviour
         Instance = this;
     }
 
+
     public Target GetTarget()
     {
+        if (pools == null || pools.Length == 0) return null;
+
         int index = Random.Range(0, pools.Length);
+        Debug.Log("Spawning from pool: " + pools[index].name);
+
         return pools[index].Get();
     }
 }
